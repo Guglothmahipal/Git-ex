189dfcf128abaa3689bf47bfa8d8054a766ac26b
Gugloth Mahipal <guglothmahipal007@gmail.com>
2024-09-16 22:14:29 +0530

    new two java files commiting

---
+++ Final_keyword.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Final_keyword {
+public static void main(String args[]){
+// without final keyword
+int a=10;
+a=a+10;
+System.out.println("a="+a+"\n");
+//with final keyword
+final int b=10;
+// b=b+10;
+System.out.println("b="+b);
+
+ }
+}
